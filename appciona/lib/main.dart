9a3cc727e01d6f45475076208308cb7804552e39
joshuaolivo <77625489+joshuaolivo@users.noreply.github.com>
2022-05-14 13:40:15 -0500

    BottonBar

--- appciona/lib/main.dart
+++ appciona/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:appciona/pages/agenda_page.dart';
+import 'package:appciona/pages/bottonbar.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -29,6 +31,9 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _page = 0;
+  GlobalKey<CurvedNavigationBarState> _bottomNavigationKey = GlobalKey();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -52,6 +57,29 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
           ],
         ),
+      ),
+      bottomNavigationBar: CurvedNavigationBar(
+        key: _bottomNavigationKey,
+        index: 0,
+        height: 60.0,
+        items: <Widget>[
+          Icon(Icons.home, size: 20),
+          Icon(Icons.play_arrow, size: 20),
+          Icon(Icons.list, size: 20),
+          Icon(Icons.message, size: 20),
+          Icon(Icons.perm_identity, size: 20),
+        ],
+        color: Colors.white,
+        buttonBackgroundColor: Colors.white,
+        backgroundColor: Colors.blueAccent,
+        animationCurve: Curves.easeInOut,
+        animationDuration: Duration(milliseconds: 600),
+        onTap: (index) {
+          setState(() {
+            _page = index;
+          });
+        },
+        letIndexChange: (index) => true,
       ),
     );
   }
