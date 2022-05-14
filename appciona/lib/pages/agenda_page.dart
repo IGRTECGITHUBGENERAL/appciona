c5662cde71bee52ae838878b8b409c3ebbe13323
RaulGonzalezIrgtec <raul.gonzalez@igrtec.com>
2022-05-14 12:43:22 -0500

    Folder pages y assets

---
+++ appciona/lib/pages/agenda_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class AgendaPage extends StatefulWidget {
+  const AgendaPage({Key? key}) : super(key: key);
+
+  @override
+  State<AgendaPage> createState() => _AgendaPageState();
+}
+
+class _AgendaPageState extends State<AgendaPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Agenda'),
+        actions: [
+          Image.asset(''),
+        ],
+      ),
+    );
+  }
+}
