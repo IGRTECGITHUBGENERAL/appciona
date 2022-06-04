abe2b183cc11748337e670492f6d1a3b73a71343
joshuaolivo <77625489+joshuaolivo@users.noreply.github.com>
2022-06-04 14:49:27 -0500

    QR funcionando

--- appciona/lib/pages/turismo/turismo.dart
+++ appciona/lib/pages/turismo/turismo.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:qrscan/qrscan.dart' as scanner;
 
 class TurismoPage extends StatefulWidget {
   const TurismoPage({Key? key}) : super(key: key);
@@ -8,18 +9,39 @@ class TurismoPage extends StatefulWidget {
 }
 
 class _TurismoPageState extends State<TurismoPage> {
+  String qrValue = "Codigo Qr";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            children: const [
-              Text('Turismo'),
-            ],
-          ),
+      body: Scaffold(
+        appBar: AppBar(
+          elevation: 2,
+          backgroundColor: Colors.white,
+          title: const Text("Turismo"),
+          actions: [
+            Image.asset(
+              'assets/images/logo-green.png',
+              fit: BoxFit.contain,
+            )
+          ],
+        ),
+        floatingActionButton: FloatingActionButton.extended(
+          onPressed: scanQr,
+          label: Text("Qr"),
+          icon: Icon(Icons.camera_alt),
+          backgroundColor: Colors.blue,
         ),
       ),
     );
+  }
+
+  void scanQr() async {
+    String? cameraScanResult = await scanner.scan();
+    setState(() {
+      qrValue = cameraScanResult ?? "Codigo Qr";
+      if (qrValue != "Codigo Qr") {
+        //REdireccionar
+      }
+    });
   }
 }
