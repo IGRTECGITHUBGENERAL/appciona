e2a031347c6e0f5599172aaa6953f26dda664ca9
joshuaolivo <77625489+joshuaolivo@users.noreply.github.com>
2022-06-04 13:46:16 -0500

    QR 2

---
+++ appciona/lib/pages/QR/LectorQR.dart
@@ -0,0 +1,14 @@
+import 'dart:developer';
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class QRViewExample extends StatefulWidget {
+  const QRViewExample({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _QRViewExampleState();
+}
+
+class _QRViewExampleState extends State<QRViewExample> {}
