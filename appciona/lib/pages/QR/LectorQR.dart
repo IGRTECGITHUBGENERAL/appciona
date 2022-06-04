3ec5a7095d7a1dc14db9401c58e50c1967404b41
joshuaolivo <77625489+joshuaolivo@users.noreply.github.com>
2022-06-04 14:18:24 -0500

    Update LectorQR.dart

--- appciona/lib/pages/QR/LectorQR.dart
+++ appciona/lib/pages/QR/LectorQR.dart
@@ -1,8 +1,5 @@
-import 'dart:developer';
-import 'dart:io';
-
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:qrscan/qrscan.dart' as scanner;
 
 class QRViewExample extends StatefulWidget {
   const QRViewExample({Key? key}) : super(key: key);
@@ -11,4 +8,18 @@ class QRViewExample extends StatefulWidget {
   State<StatefulWidget> createState() => _QRViewExampleState();
 }
 
-class _QRViewExampleState extends State<QRViewExample> {}
+class _QRViewExampleState extends State<QRViewExample> {
+  String qrValue = "Codigo Qr";
+  void scanQr() async {
+    String? cameraScanResult = await scanner.scan();
+    setState(() {
+      qrValue = cameraScanResult ?? "Codigo Qr";
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
