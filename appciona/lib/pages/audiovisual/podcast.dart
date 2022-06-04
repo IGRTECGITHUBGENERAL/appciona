1d5acdbe7645e3c7e329edcbd2db01c722a50265
RaulGonzalezIrgtec <raul.gonzalez@igrtec.com>
2022-06-04 13:47:07 -0500

    Proceso de construcción de página de podcast

--- appciona/lib/pages/audiovisual/podcast.dart
+++ appciona/lib/pages/audiovisual/podcast.dart
@@ -1,5 +1,6 @@
 import 'package:appciona/pages/audiovisual/podcast_controller.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:date_format/date_format.dart';
 import 'package:flutter/material.dart';
 
 class PodcastPage extends StatefulWidget {
@@ -38,22 +39,106 @@ class _PodcastPageState extends State<PodcastPage> {
               builder: (context, data) {
                 if (data.hasData) {
                   DocumentSnapshot doc = data.data as DocumentSnapshot;
+                  Timestamp t = doc["FechaPublicacion"];
+                  DateTime d = t.toDate();
                   return Column(
                     children: [
                       Row(
                         children: [
-                          ClipRRect(
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(10),
+                          Container(
+                            decoration: const BoxDecoration(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(10),
+                              ),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey,
+                                  spreadRadius: 2,
+                                  blurRadius: 5,
+                                ),
+                              ],
                             ),
-                            child: Image.network('${doc["Imagen"]}'),
+                            child: ClipRRect(
+                              borderRadius: const BorderRadius.all(
+                                Radius.circular(10),
+                              ),
+                              child: Image.network(
+                                '${doc["Imagen"]}',
+                                width: size.width * 0.40,
+                              ),
+                            ),
                           ),
-                          Text('${doc["Titulo"]}'),
+                          Expanded(
+                            child: Padding(
+                              padding: const EdgeInsets.all(10.0),
+                              child: Text(
+                                '${doc["Titulo"]}',
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18,
+                                ),
+                              ),
+                            ),
+                          ),
                         ],
                       ),
-                      Text('Fecha de publicación: ${doc["FechaPublicacion"]}'),
-                      Text('Descripción\n${doc["Descripcion"]}'),
-                      Text('${doc["Link"]}'),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 15.0),
+                        child: RichText(
+                          text: TextSpan(
+                            style: const TextStyle(color: Colors.black),
+                            children: [
+                              const TextSpan(
+                                text: 'Fecha de publicación: ',
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                              TextSpan(
+                                text: formatDate(
+                                  d,
+                                  [
+                                    dd,
+                                    "-",
+                                    mm,
+                                    "-",
+                                    yyyy,
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Container(
+                        decoration: const BoxDecoration(
+                          color: Color(0XFF007474),
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(20),
+                          ),
+                        ),
+                        padding: const EdgeInsets.all(20.0),
+                        child: Column(
+                          children: [
+                            const Text(
+                              'Descripción',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 28,
+                              ),
+                            ),
+                            const Divider(
+                              color: Colors.white,
+                            ),
+                            Text(
+                              '${doc["Descripcion"]}',
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
                     ],
                   );
                 } else if (data.hasError) {
